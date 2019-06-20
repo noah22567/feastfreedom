b1aad84ef3f4eb875fd36b4a48c0f8a836afd747
noah22567 <thenoahsalas@gmail.com>
2019-06-20 16:50:05 -0400

    added auth user to settings

--- Feast-Freedom/provider/models.py
+++ Feast-Freedom/provider/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from users.models import myUser
 from django.contrib.auth.models import User
 import os
+from users.models import myUser
 # Create your models here.
 
 
-class ServiceProvider(myUser):
-
+class ServiceProvider():
+    id = models.ForeignKey(myUser,)
     ServiceProviderName = models.CharField(('Service provider name'),max_length=20)
 
 
 class Kitchen(models.Model):
 
-    # userID = models.ForeignKey(User,on_delete=models.CASCADE)
+    userID = models.ForeignKey(myUser,on_delete=models.CASCADE)
 
     Starttime = models.TimeField(('Start Time'),auto_now=False,blank=False)
     Endtime = models.TimeField(('End Time'),auto_now=False,blank=False)
